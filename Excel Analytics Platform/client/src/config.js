cb7005278bcef5ac35ce4697dc1143faf9c62a79
Jyoti <jyotikiran58439@gmail.com>
2026-02-27 22:58:33 +0530

    Fixed double api issue in config

--- Excel Analytics Platform/client/src/config.js
+++ Excel Analytics Platform/client/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 // Backend API URL
-export const API_URL = "https://excel-analytics-platform-vj3o.onrender.com/api";
+export const API_URL = "https://excel-analytics-platform-vj3o.onrender.com";
 
 // Optional: default constants
 export const APP_NAME = "Excel Analytics Platform";
